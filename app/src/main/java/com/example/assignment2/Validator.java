7638f4dd9d8668f338b347edc6d5d53e01711b8b
Nicholas Mingo <nic.mingo@icloud.com>
2018-06-05 11:40:41 -0300

    a2-stage1

---
+++ app/src/main/java/com/example/assignment2/Validator.java
@@ -0,0 +1,14 @@
+package com.example.assignment2;
+
+public class Validator {
+    // validate method
+    public int validate(String password) {
+        int x = 0;
+        if(password.length() >= 8 ) {
+            x++;
+        }if(!(password.equalsIgnoreCase("password"))) {
+            x++;
+        }
+        return x;
+    }
+}
