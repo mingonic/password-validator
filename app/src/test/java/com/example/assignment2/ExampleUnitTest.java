7aa7618b79a3900ab2013964918350ad74095aa6
Nicholas Mingo <nic.mingo@icloud.com>
2018-06-05 14:35:16 -0300

    added JUnit tests for section a2-stage2

--- app/src/test/java/com/example/assignment2/ExampleUnitTest.java
+++ app/src/test/java/com/example/assignment2/ExampleUnitTest.java
@@ -18,16 +18,43 @@ public class ExampleUnitTest {
         v = new Validator();
     }
 
+    //checking to see that it is not the string "password"
     @Test
     public void is_not_password() {
         assertEquals(v.validate("hello"),1);
     }
 
+    //Password contains a special character
+    @Test
+    public void is_Special() {
+        String s = "H$ll0!!!";
+
+        //assertEquals(v.validate(s),1);
+    }
+
+    //Password contains a number
+    @Test
+    public void is_a_number() {
+        String s = "B3lltone";
+
+        //assertEquals(v.validate(s),1);
+    }
+
+    //Password contains a uppercase and a lower case
+    @Test
+    public void is_UPPER_and_lower() {
+        String s = "HaHaHaHaHaHaHa";
+
+        //assertEquals(v.validate(s),1);
+    }
+
+    //Password is at least 8 characters long
     @Test
     public void is_long() {
         assertEquals(v.validate("password"),1);
     }
 
+    //Password passes all tests
     @Test
     public void all_tests(){
         assertEquals(v.validate("arithmetic"),2);
